bf0b5acccf894b603a7aace3e306081dcfbaff7e
Nadinnnka <tymoshyk02@gmail.com>
2024-09-17 19:40:33 +0300

    make build

---
+++ index.2e573dc3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientWidth,l=t.clientHeight;e.style.top="".concat((l-e.clientHeight)/2,"px"),e.style.left="".concat((c-e.clientWidth)/2,"px");
+//# sourceMappingURL=index.2e573dc3.js.map
